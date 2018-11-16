de9992686afda59c27fcaccbe082ccc9664035cc
Nick Kutryj <nick@tokagen.com>
2018-11-16 15:00:26 +0100

    Migrations

--- apps/infrastructure/priv/repository/migrations/20181026001345_create_assets.exs
+++ apps/infrastructure/priv/repository/migrations/20181026001345_create_assets.exs
@@ -4,7 +4,10 @@ defmodule Infrastructure.Repository.Migrations.CreateAssets do
   def change do
     create table(:assets, primary_key: false) do
       add(:id, :binary_id, primary_key: true)
+      add(:name, :string, null: false)
+      add(:extension, :string, null: false)
       add(:type, :string, null: false)
+      add(:uploaded, :boolean, null: false, default: false)
       timestamps()
     end
   end
--- apps/infrastructure/priv/repository/migrations/20181026023245_create_asset_conversions.exs
+++ apps/infrastructure/priv/repository/migrations/20181026023245_create_asset_conversions.exs
@@ -4,13 +4,10 @@ defmodule Infrastructure.Repository.Migrations.CreateAssetConversions do
   def change do
     create table(:asset_conversions, primary_key: false) do
       add(:id, :binary_id, primary_key: true)
-      add(:finished, :boolean, null: false, default: false)
-      add(:sent, :boolean, null: false, default: false)
-
       add(:name, :string, null: false)
-
+      add(:extension, :string, null: false)
+      add(:type, :string, null: false)
       add(:asset_id, references(:assets, type: :binary_id, on_delete: :delete_all))
-
       timestamps()
     end
 
