61b85cc24ebed7c9304efbe7ea1562c6f4523a13
szax <szax22@gmail.com>
2018-11-29 15:41:48 +0100

    add point repository

---
+++ apps/domain/lib/adventure/repository/point.ex
@@ -0,0 +1,16 @@
+defmodule Domain.Adventure.Repository.Point do
+  @moduledoc """
+  Repository start adventure
+  """
+  use Infrastructure.Repository.Models
+  import Ecto.Query
+  alias Infrastructure.Repository
+  alias Ecto.Multi
+
+  def check_point_position() do
+  end
+
+  def marke_point() do
+  end
+
+end
