676cbccb4ee3ca28f60aa35a29267abdcd1e2e9c
szax <szax22@gmail.com>
2019-01-28 16:35:28 +0100

    fix makro in helper view

--- apps/creator_api/lib/creator_api_web.ex
+++ apps/creator_api/lib/creator_api_web.ex
@@ -40,7 +40,7 @@ defmodule CreatorApiWeb do
       import CreatorApiWeb.ErrorHelpers
       import CreatorApiWeb.Gettext
       alias CreatorApiWeb.Router.Helpers, as: Routes
-      use CreatorApiWeb.ViewHelpers
+      import CreatorApiWeb.ViewHelpers
     end
   end
 
--- apps/creator_api/lib/creator_api_web/view_helpers.ex
+++ apps/creator_api/lib/creator_api_web/view_helpers.ex
@@ -1,10 +1,5 @@
 defmodule CreatorApiWeb.ViewHelpers do
   alias Infrastructure.Service.AssetManagement
-  defmacro __using__(_opts) do
-    quote do
-      import unquote(__MODULE__)
-    end
-  end
 
   def asset_url(nil) do
     nil
