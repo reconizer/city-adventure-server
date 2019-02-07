fffc005b4b205f44f198831edd2bcb207bef9237
Nick Kutryj <nick@tokagen.com>
2019-02-07 14:14:04 +0100

    Packaging fix

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -7,10 +7,10 @@ MIX_ENV=prod mix do compile, release
 mkdir release
 mkdir release/content
 mkdir release/scripts
-cp _build/prod/rel/gameinn/releases/*/app.tar.gz release/content/app.tar.gz
+cp _build/prod/rel/app/releases/*/app.tar.gz release/content/app.tar.gz
 cp appspec.yml release/appspec.yml
 cp -a scripts/. release/scripts/
 cd release
-tar  --owner=1000 --group=1000 -zcvf ../gameinn.tar.gz .
+tar  --owner=1000 --group=1000 -zcvf ../release.tar.gz .
 cd ..
 # fi
