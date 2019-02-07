504ae24f39773b870af22aa01ef56af14670cdb0
Nick Kutryj <nick@tokagen.com>
2019-02-07 13:00:18 +0100

    Fix pre-installation script

--- scripts/travis/before-install.sh
+++ scripts/travis/before-install.sh
@@ -2,3 +2,6 @@
 set -ev
 
 pip install --user awscli
+sudo sed -i -e '/local.*peer/s/postgres/all/' -e 's/peer\|md5/trust/g' /etc/postgresql/*/main/pg_hba.conf
+sudo service postgresql restart
+sleep 1
