56c3811495db3a4b9326afeba294959a307f4435
Nick Kutryj <nick@tokagen.com>
2019-02-07 12:12:53 +0100

    Fix password for ECTO

--- apps/infrastructure/config/test.exs
+++ apps/infrastructure/config/test.exs
@@ -5,6 +5,7 @@ config :infrastructure, Infrastructure.Repository,
   username: "postgres",
   database: "city_adventure_test",
   hostname: "localhost",
+  password: "",
   port: 5433,
   types: Infrastructure.Repository.PostgresTypes,
   pool: Ecto.Adapters.SQL.Sandbox
--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,13 +1,21 @@
 use Mix.Config
 
-db_port = String.to_integer(System.get_env("DATABASE_PORT"))
+db_port = String.to_integer(System.get_env("CA.INFRASTRUCTURE.DATABASE_PORT"))
 
 config :infrastructure, Infrastructure.Repository,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DATABASE_USER"),
-  password: System.get_env("DATABASE_PASSWORD"),
-  database: System.get_env("DATABASE_NAME"),
-  hostname: System.get_env("DATABASE_HOST"),
+  username: System.get_env("CA.INFRASTRUCTURE.DATABASE_USER"),
+  password: System.get_env("CA.INFRASTRUCTURE.DATABASE_PASSWORD"),
+  database: System.get_env("CA.INFRASTRUCTURE.DATABASE_NAME"),
+  hostname: System.get_env("CA.INFRASTRUCTURE.DATABASE_HOST"),
   port: db_port,
+  pool_size: 10,
   types: Infrastructure.Repository.PostgresTypes,
-  pool: Ecto.Adapters.SQL.Sandbox
+
+config :infrastructure, :asset_bucket, System.get_env("CA.INFRASTRUCTURE_ASSET_BUCKET")
+
+config :infrastructure, :google_client_id, System.get_env("CA.INFRASTRUCTURE.GOOGLE_CLIENT_ID")
+config :infrastructure, :google_client_secret, System.get_env("CA.INFRASTRUCTURE.GOOGLE_CLIENT_SECRET")
+config :infrastructure, :google_refresh_token, System.get_env("CA.INFRASTRUCTURE.GOOGLE_REFRESH_TOKEN")
+config :infrastructure, :google_refresh_token_url, System.get_env("CA.INFRASTRUCTURE.GOOGLE_REFRESH_TOKEN_URL")
+config :infrastructure, :google_play_api_url, System.get_env("CA.INFRASTRUCTURE.GOOGLE_PLAY_API_URL")
