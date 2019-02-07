83e3bacf66666c47879022444144a16fe151a364
Nick Kutryj <nick@tokagen.com>
2019-02-07 11:23:16 +0100

    Database env setup

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,11 +1,13 @@
 use Mix.Config
 
+db_port = String.to_integer(System.get_env("DATABASE_PORT"))
+
 config :infrastructure, Infrastructure.Repository,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "city_adventure_test",
-  hostname: "localhost",
-  port: 5433,
+  username: System.get_env("DATABASE_USER"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  database: System.get_env("DATABASE_NAME"),
+  hostname: System.get_env("DATABASE_HOST"),
+  port: db_port,
   types: Infrastructure.Repository.PostgresTypes,
   pool: Ecto.Adapters.SQL.Sandbox
