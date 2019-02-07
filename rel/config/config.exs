16e6a9fde6e0aa10d7431f925830de15563cece1
Nick Kutryj <nick@tokagen.com>
2019-02-07 13:44:09 +0100

    Config fix

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -19,3 +19,36 @@ config :infrastructure, :google_client_secret, System.get_env("CA_INFRASTRUCTURE
 config :infrastructure, :google_refresh_token, System.get_env("CA_INFRASTRUCTURE_GOOGLE_REFRESH_TOKEN")
 config :infrastructure, :google_refresh_token_url, System.get_env("CA_INFRASTRUCTURE_GOOGLE_REFRESH_TOKEN_URL")
 config :infrastructure, :google_play_api_url, System.get_env("CA_INFRASTRUCTURE_GOOGLE_PLAY_API_URL")
+
+config :phoenix, :serve_endpoints, true
+
+administration_api_port = String.to_integer(System.get_env("CA_ADMINISTRATION_API_PORT"))
+user_api_port = String.to_integer(System.get_env("CA_USER_API_PORT"))
+creator_api_port = String.to_integer(System.get_env("CA_CREATOR_API_PORT"))
+
+config :administration_api, AdministrationApiWeb.Endpoint,
+  http: [port: administration_api_port],
+  url: [host: "localhost", port: administration_api_port]
+
+config :creator_api, CreatorApiWeb.Endpoint,
+  http: [port: creator_api_port],
+  url: [host: "localhost", port: creator_api_port]
+
+config :user_api, UserApiWeb.Endpoint,
+  http: [port: user_api_port],
+  url: [host: "localhost", port: user_api_port]
+
+config :user_api,
+  secret: System.get_env("CA_USER_API_SECRET")
+
+config :user_api, CreatorApiWeb.Endpoint, secret_key_base: System.get_env("CA_USER_API_SECRET_KEY_BASE")
+
+config :creator_api, CreatorApiWeb.Endpoint, secret_key_base: System.get_env("CA_CREATOR_API_SECRET_KEY_BASE")
+
+config :administration_api, AdministrationApiWeb.Endpoint, secret_key_base: System.get_env("CA_ADMINITRATION_API_SECRET_KEY_BASE")
+
+config :media_storage,
+  s3_access_key_id: System.get_env("CA_MEDIA_STORAGE_S3_ACCESS_KEY_ID"),
+  s3_secret_access_key: System.get_env("CA_MEDIA_STORAGE_S3_SECRET_ACCESS_KEY"),
+  s3_region: System.get_env("CA_MEDIA_STORAGE_S3_REGION"),
+  s3_bucket: System.get_env("CA_INFRASTRUCTURE_ASSET_BUCKET")
