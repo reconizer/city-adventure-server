d0984a21192de9ddb6dabc0ccb0ff973ee5d605a
Nick Kutryj <nick@tokagen.com>
2019-02-07 13:49:15 +0100

    Skip test phase

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -3,8 +3,6 @@ set -ev
 echo "PREPARING DEPLOYMENT"
 
 # if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && ([ "${TRAVIS_BRANCH}" == "staging-deployment" ] ; then
-# aws s3 cp s3://gameinn-deployments/unfold-server/$TRAVIS_BRANCH/config.tar.gz /tmp/config.tar.gz
-# tar -xvf /tmp/config.tar.gz
 MIX_ENV=prod mix do compile, release
 mkdir release
 mkdir release/content
--- scripts/travis/test.sh
+++ scripts/travis/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ev
 
-MIX_ENV=test mix ecto.migrate -r Infrastructure.Repository
-MIX_ENV=test mix test
+# MIX_ENV=test mix ecto.migrate -r Infrastructure.Repository
+# MIX_ENV=test mix test
 
-if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && ([ "${TRAVIS_BRANCH}" == "staging-deployment" ]) ; then
-  MIX_ENV=test mix docs
-  aws s3 cp doc s3://gameinn-docs/ --recursive
-fi
+# if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && ([ "${TRAVIS_BRANCH}" == "staging-deployment" ]) ; then
+#   MIX_ENV=test mix docs
+#   aws s3 cp doc s3://gameinn-docs/ --recursive
+# fi
