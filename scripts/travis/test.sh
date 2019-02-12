e43821692e9b426241153cf2de5d0073a7935e8a
Nick Kutryj <nick@tokagen.com>
2019-02-12 11:48:43 +0100

    Run travis tests for each app separetely

--- scripts/travis/test.sh
+++ scripts/travis/test.sh
@@ -2,10 +2,11 @@
 set -ev
 cp apps/infrastructure/config/travis.exs apps/infrastructure/config/test.exs
 
-MIX_ENV=test mix ecto.migrate -r Infrastructure.Repository
-MIX_ENV=test mix test
+mix ecto.migrate -r Infrastructure.Repository
+mix test
+echo "Re-running tests for each app" ; for app in apps/**; do cd $app && mix test; cd ../.. ; done
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && ([ "${TRAVIS_BRANCH}" == "staging-deployment" ]) ; then
-  MIX_ENV=test mix docs
+  mix docs
   aws s3 cp doc s3://gameinn-docs/ --recursive
 fi
