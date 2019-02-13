789764910f4580f6ef13f20e028b09c3abc45443
Nick Kutryj <nick@tokagen.com>
2019-02-13 10:56:14 +0100

    Fix asset management

--- apps/infrastructure/lib/service/asset_management.ex
+++ apps/infrastructure/lib/service/asset_management.ex
@@ -2,8 +2,6 @@ defmodule Infrastructure.Service.AssetManagement do
   alias Infrastructure.Repository.Models.Asset
   alias Infrastructure.Repository.Models.AssetConversion
 
-  @bucket Application.get_env(:infrastructure, :asset_bucket)
-
   @spec download_url(Asset.t() | AssetConversion.t()) :: {:error, String.t()} | {:ok, String.t()}
   def download_url(%Asset{id: id, type: type, extension: extension, name: name, uploaded: true}) do
     download_url(type, id, name, extension)
@@ -24,7 +22,7 @@ defmodule Infrastructure.Service.AssetManagement do
       |> Path.join()
 
     "#{path}.#{extension}"
-    |> MediaStorage.download_url(bucket: @bucket)
+    |> MediaStorage.download_url(bucket: bucket())
   end
 
   @spec upload_url(String.t(), String.t(), String.t()) :: {:error, String.t()} | {:ok, String.t()}
@@ -34,6 +32,10 @@ defmodule Infrastructure.Service.AssetManagement do
       |> Path.join()
 
     "#{path}.#{extension}"
-    |> MediaStorage.upload_url(bucket: @bucket)
+    |> MediaStorage.upload_url(bucket: bucket())
+  end
+
+  def bucket do
+    Application.get_env(:infrastructure, :asset_bucket)
   end
 end
