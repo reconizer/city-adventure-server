e4826339da2eedb1dccaa713bfd0e75989a597a4
Nick Kutryj <nick@tokagen.com>
2019-02-14 11:24:30 +0100

    Fix 404's

--- apps/user_api/lib/user_api_web/endpoint.ex
+++ apps/user_api/lib/user_api_web/endpoint.ex
@@ -15,8 +15,6 @@ defmodule UserApiWeb.Endpoint do
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
-    plug(Phoenix.LiveReloader)
     plug(Phoenix.CodeReloader)
   end
 
