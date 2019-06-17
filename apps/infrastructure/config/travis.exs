158b326fc07b4d156abee6e59ddb6b561e4899b3
szax <szax22@gmail.com>
2019-06-17 10:34:08 +0200

    config media storage

--- apps/infrastructure/config/travis.exs
+++ apps/infrastructure/config/travis.exs
@@ -10,4 +10,10 @@ config :infrastructure, Infrastructure.Repository,
   types: Infrastructure.Repository.PostgresTypes,
   pool: Ecto.Adapters.SQL.Sandbox
 
+config :media_storage,
+  s3_access_key_id: "",
+  s3_secret_access_key: "",
+  s3_region: "",
+  s3_bucket: ""
+
 config :infrastructure, :asset_bucket, "gameinn-2-assets"
